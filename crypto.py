eb07eade2a15d45bf606d0c1ccb450603f228870
IsaqueB <67600830+IsaqueB@users.noreply.github.com>
2020-09-16 18:15:25 -0300

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,72 @@
+
+def en_cesar(st,k):
+    st_cesar = []
+    for c in st:
+        st_cesar.append(chr(ord(c)+int(k)%55291))
+    return ''.join(st_cesar)
+
+def dec_cesar(st,k):
+    st_cesar = []
+    for c in st:
+        st_cesar.append(chr(ord(c)-int(k)%55291))
+    return ''.join(st_cesar)
+
+def en_xor(arr,k):
+    bk=0
+    chrkey=0
+    for element in k:
+        if element=='0':
+            bk=bk*2
+        elif element=='1':
+            bk=bk*2+1
+        else:
+            chrkey=1
+  #print("bk:"+bin(bk))
+    if chrkey==0:
+        xarr=[]
+        for c in arr:
+            hx=str(hex(ord(c)^bk))
+            xarr.append(hx[2:])
+
+        return ''.join(xarr)
+    else:
+        xarr=[]
+        i=0
+        for c in arr:
+            hx=str(hex(ord(c)^ord(k[i%len(k)])))
+            hnum=hx[2:]
+            if(len(hnum)<2):
+                hnum='0'+hnum
+            xarr.append(hnum)
+            i=i+1
+
+        return ''.join(xarr)
+
+
+def dec_xor(arr,k):
+    bk=0
+    for element in k:
+        if element=='0':
+            bk=bk*2
+        elif element=='1':
+            bk=bk*2+1
+        else:
+            chrkey=1
+    #print("bk:"+bin(bk))
+    if chrkey==0:
+        xarr=[]
+        for x in range(int(len(arr)/2)):
+            hexs="0x"+arr[2*x]+arr[2*x+1]
+            hexi=int(hexs, 16)
+            xarr.append(chr(hexi^bk))
+        return ''.join(xarr)
+    else:
+        xarr=[]
+       # i=0
+        for x in range(int(len(arr)/2)):
+            hexs="0x"+arr[2*x]+arr[2*x+1]
+            hexi=int(hexs, 16)
+            xarr.append(chr(hexi^ord(k[x%len(k)])))
+        return ''.join(xarr)
+
+
